508bcf7b6894a32ae1dd126c67c5cfbcf1468634
Pierre Jean Heissat <pjn.heissat@gmail.com>
2020-01-28 21:17:44 +0100

    Modifs du nom de colonne url

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_192841) do
+ActiveRecord::Schema.define(version: 2020_01_28_201324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "audios", force: :cascade do |t|
+    t.text "text_url"
+    t.string "language"
+    t.text "text_to_transcript"
+    t.text "audio_url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_audios_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_01_28_192841) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "audios", "users"
 end

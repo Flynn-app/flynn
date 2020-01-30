3a3b8e3879f9500d26f20984fcfabf460aec052d
Nicolas Boulet <nicolasboulet@live.fr>
2020-01-30 19:21:54 +0100

    route audio new

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -1,5 +1,7 @@
 class AudiosController < ApplicationController
   def new
+    @audio = Audio.new
+    authorize @audio
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'audios/new'
-  get 'audios/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

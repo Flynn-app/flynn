1b803907beedefc74123260eb1b063515ff72fd0
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-01 16:49:59 +0100

    add iso in audio model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_133001) do
+ActiveRecord::Schema.define(version: 2020_02_01_154825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_02_01_133001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "iso"
     t.index ["user_id"], name: "index_audios_on_user_id"
   end
 

7f4eba1d4bde1675debe359a2785c4052ef9d959
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-02 15:07:51 +0100

    Add Ap1 base controller2

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::BaseController < ActionController::API
   include Pundit
 
-  after_action :verify_authorized#, except: :index
-  after_action :verify_policy_scoped#, only: :index
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 
   rescue_from StandardError,                with: :internal_server_error
   rescue_from Pundit::NotAuthorizedError,   with: :user_not_authorized

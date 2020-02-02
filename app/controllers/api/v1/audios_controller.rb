c391e7a9620748f2098a9b33c2a38c6016cffa65
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-02 15:44:02 +0100

    api show controller route

--- app/controllers/api/v1/audios_controller.rb
+++ app/controllers/api/v1/audios_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::AudiosController < Api::V1::BaseController
+  before_action :set_audio, only: [ :show ]
+
   def index
     @audios = policy_scope(Audio)
   end
+
+  private
+
+  def set_audio
+    @audio = Audio.find(params[:id])
+    authorize @audio
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :audios, only: [ :create, :index ]
+      resources :audios, only: [ :create, :index, :show ]
     end
   end
 end

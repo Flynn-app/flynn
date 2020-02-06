7c7a9653a8efa53c3bbe1b2ab1cdc0cd29824a73
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-06 20:46:00 +0100

    correct issue controller

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -3,7 +3,6 @@ class AudiosController < ApplicationController
   require 'nokogiri'
   require 'whatlanguage'
 
-
   def new
     @audio = Audio.new
     authorize @audio
@@ -11,18 +10,20 @@ class AudiosController < ApplicationController
 
   def create
     @audio = Audio.new(audio_params)
-
     @audio.user = current_user
 
-    if (defined? params[:text_to_transcript]) && (defined? params[:title])
-      @audio.title = params[:title].to_s
-      @audio.text_to_transcript = params[:text_to_transcript]
+    if params[:audio][:text_to_transcript].present? && params[:audio][:title].present?
+      # @audio.title = params[:audio][:title]
+      # @audio.text_to_transcript = params[:audio][:text_to_transcript]
 
       wl = WhatLanguage.new(:all)
 
       @audio.language = wl.language(@audio.text_to_transcript).to_s
       @audio.iso = wl.language_iso(@audio.text_to_transcript).to_s
-    elsif (defined? params[:text_url])
+
+    elsif (params[:audio][:text_url].present?)
+
+      @audio.text_url = params[:audio][:text_url]
       content = URI.open(@audio.text_url).read
       html_doc = Nokogiri::HTML(content)
 
@@ -30,18 +31,19 @@ class AudiosController < ApplicationController
 
       text_content = Boilerpipe::Extractors::ArticleExtractor.text(content)
       @audio.text_to_transcript = text_content
+
       wl = WhatLanguage.new(:all)
       @audio.language = wl.language(@audio.text_to_transcript).to_s
       @audio.iso = wl.language_iso(@audio.text_to_transcript).to_s
     end
       filename = SynthesizeText.new(@audio.text_to_transcript).synthesize_text
       # file_output = "public/output/output.mp3"
-
       # Cloudinary::Uploader.upload(filename, resource_type: :video)
       File.open(filename, "r") do |file|
         @audio.audiofile.attach(io: file, filename: filename)
         File.delete(file)
       end
+
     if @audio.save
       # redirect_to root_path
       redirect_to audio_path(@audio)

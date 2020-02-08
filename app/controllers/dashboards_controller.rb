6374f28528c1f6856a63535161418ae48f62611e
quentinlallemand <quentinlallemand@gmail.com>
2020-02-08 17:58:49 +0100

    push plz

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,10 +3,11 @@ class DashboardsController < ApplicationController
 
   def profile
     skip_authorization
-    @user = current_user
+    @user = User.friendly.find(current_user.id)
   end
 
   def public_profile
     skip_authorization
+    @user = User.friendly.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend FriendlyId
+  friendly_id :nickname, use: :slugged
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :audios, dependent: :destroy

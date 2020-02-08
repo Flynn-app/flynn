efa26061e9832c4bc7d2b3c6cc34a2052c1ffa32
Pierre Jean Heissat <pjn.heissat@gmail.com>
2020-02-08 14:56:52 +0100

    Boutons sidebar

--- app/assets/javascripts/toggle_active.js
+++ app/assets/javascripts/toggle_active.js
@@ -1,9 +1,9 @@
-const liens = document.querySelectorAll('.sidebar_link');
+// const liens = document.querySelectorAll('.sidebar_link');
 
-liens.forEach(lien =>
-  lien.addEventListener("click", (event) => {
-  document.querySelectorAll('.active').forEach(el =>
-  el.classList.remove('active'));
-  handleClick(event.currentTarget);
-  event.currentTarget.classList.add("active");
-}));
+// liens.forEach(lien =>
+//   lien.addEventListener("click", (event) => {
+//   document.querySelectorAll('.active').forEach(el =>
+//   el.classList.remove('active'));
+//   handleClick(event.currentTarget);
+//   event.currentTarget.classList.add("active");
+// }));

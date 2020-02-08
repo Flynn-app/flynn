d87fa5928ce208fcdaf37f90c398ef3a93ccb502
quentinlallemand <quentinlallemand@gmail.com>
2020-02-08 16:59:03 +0100

    controller + policy

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ User.create!(
   email: "test@test.com",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  nickname: Faker::Internet.username,
+  nickname: '@'+Faker::Internet.username,
   password:  '123456',
   password_confirmation: '123456',
   user_bio: Faker::Quote.matz
@@ -19,7 +19,7 @@ User.create!(
   email: "test2@test.com",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  nickname: Faker::Internet.username,
+  nickname: '@'+Faker::Internet.username,
   password:  '123456',
   password_confirmation: '123456',
   user_bio: Faker::Quote.matz
@@ -30,7 +30,7 @@ User.create!(
   email: "test3@test.com",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  nickname: Faker::Internet.username,
+  nickname: '@'+Faker::Internet.username,
   password:  '123456',
   password_confirmation: '123456',
   user_bio: Faker::Quote.matz

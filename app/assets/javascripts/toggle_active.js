2c877a8afba760a3b2aee9241f11274a8636412a
Pierre Jean Heissat <pjn.heissat@gmail.com>
2020-02-08 14:10:50 +0100

    JS en cours

---
+++ app/assets/javascripts/toggle_active.js
@@ -0,0 +1,9 @@
+const liens = document.querySelectorAll('.sidebar_link');
+
+liens.forEach(lien =>
+  lien.addEventListener("click", (event) => {
+  document.querySelectorAll('.active').forEach(el =>
+  el.classList.remove('active'));
+  handleClick(event.currentTarget);
+  event.currentTarget.classList.add("active");
+}));

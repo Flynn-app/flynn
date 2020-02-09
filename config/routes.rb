96b5f4d3b2fba3d92fd89ff9afeb40702d9b30ff
quentinlallemand <quentinlallemand@gmail.com>
2020-02-09 17:39:25 +0100

    user show + first component

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
   resources :audios, only: [:new, :create, :show]
-  scope "/home" do
-    resources :users
-  end
+  # scope "/home" do
+  #   resources :users
+  # end
 
   devise_for :users
   root to: 'pages#home'
+  resources :users
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do

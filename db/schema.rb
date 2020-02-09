bb769da42e15237f55a2820929dfe90787e0c4cb
quentinlallemand <quentinlallemand@gmail.com>
2020-02-09 20:18:02 +0100

    add text-image + system that get image page from article"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_133100) do
+ActiveRecord::Schema.define(version: 2020_02_09_181830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_133100) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "iso"
+    t.string "text_image"
     t.index ["user_id"], name: "index_audios_on_user_id"
   end
 

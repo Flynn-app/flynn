932b24e7b6f1da0cf5fbee0dbb7cead7e27eb8d8
quentinlallemand <quentinlallemand@gmail.com>
2020-02-11 23:15:52 +0100

    ajax on player youhouuuu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     authorize @user
+
+    @audio = Audio.find(params[:audio]) if params[:audio]
+    @url = @audio.audio_url if params[:audio]
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,17 @@
 
 import "bootstrap";
 
-import { player } from '../plugins/init_plyr';
+// import { player } from '../plugins/init_plyr';
 
+import 'plyr/dist/plyr.css';
+import Plyr from 'plyr';
 
+const player = new Plyr(document.getElementById('player'), {
+  autoplay: false,
+  invertTime: true,
+  toggleInvert: true,
+  tooltips: { controls: false, seek: false },
+  control: 'settings',
+});
+
+// export { player };

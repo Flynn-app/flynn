21333da839ad87942065f54bca809f98ba13be00
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-12 21:48:57 +0100

    add event listener on mercury

--- app/javascript/plugins/init_mercury.js
+++ app/javascript/plugins/init_mercury.js
@@ -2,24 +2,23 @@ import Mercury from '@postlight/mercury-parser';
 
 const buttonMercury = document.getElementById("mercuryButton");
 
+buttonMercury.addEventListener(click, event () => {
 
-const cors = 'https://cors-anywhere.herokuapp.com/';
-let targetSite = document.getElementById("mercuryInput").value;
-const url = `${cors}${targetSite}`;
+  const cors = 'https://cors-anywhere.herokuapp.com/';
+  let targetSite = document.getElementById("mercuryInput").value;
+  const url = `${cors}${targetSite}`;
 
-const sendData = (data) => {
-  // const site_url = window.location.href;
-  const url = 'http://127.0.0.1:3000/api/v1/audios';
-    fetch(url, {
-    method: 'POST',
-    headers: { "Content-Type": "application/json",
-               "Accept": "application/json"
-     },
-    body: JSON.stringify({ "audio": { "title": `${data.title}`,
-                                      "content": `${data.content}`
-    } } )
-  })
-}
-
-
-// Mercury.parse(url).then(result => sendData(result);
+  const sendData = (data) => {
+    const url = 'http://127.0.0.1:3000/api/v1/audios';
+      fetch(url, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json",
+                 "Accept": "application/json"
+       },
+      body: JSON.stringify({ "audio": { "title": `${data.title}`,
+                                        "content": `${data.content}`
+      } } )
+    })
+  }
+  // Mercury.parse(url).then(result => sendData(result);
+})

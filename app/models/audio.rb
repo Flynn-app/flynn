33bae7f46941694b06cf7610f5194a061c200fc4
quentinlallemand <quentinlallemand@gmail.com>
2020-02-15 23:40:20 +0100

    dependency works on creation

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,6 +1,7 @@
 class Audio < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :playlist, through: :audios_playlists
+  has_many :audios_playlists, dependent: :destroy
+  has_many :playlists, through: :audios_playlists
   # has_one_attached :audiofile
 
   FILE_EXTENSION = 'mp3'.freeze

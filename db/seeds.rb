031303978ecf83b3fe9847b01b7fd733e5cba834
quentinlallemand <quentinlallemand@gmail.com>
2020-02-16 17:33:26 +0100

    edit playlist section design

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ User.create!(
   email: "test@test.com",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  nickname: '@John_Preston',
+  nickname: '@john_preston',
   password:  '123456',
   password_confirmation: '123456',
   user_bio: Faker::Quote.matz
@@ -26,7 +26,7 @@ User.create!(
   email: "test2@test.com",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  nickname: '@Martin_Sanchez',
+  nickname: '@martin123',
   password:  '123456',
   password_confirmation: '123456',
   user_bio: Faker::Quote.matz
@@ -37,7 +37,7 @@ User.create!(
   email: "test3@test.com",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  nickname: '@Salome_Rakhia',
+  nickname: '@juliaB',
   password:  '123456',
   password_confirmation: '123456',
   user_bio: Faker::Quote.matz
@@ -69,14 +69,14 @@ puts "Playlists created"
 puts "Create Audios"
 
 Audio.create!(
-  text_url: "http://www.footmercato.net/flash-mercato/manchester-city-aymeric-laporte-previent-le-real-madrid_273705",
+  text_url: "http://www.footmercato.net/autre-championnat/erling-braut-haland-la-machine-a-buts-du-borussia-dortmund_273993",
   language: "french",
-  text_to_transcript: "Manchester City : Aymeric Laporte prévient le Real Madrid\nManchester City : Aymeric Laporte prévient le Real Madrid\nPublié le :\nLucas Billard Suivre @@LucasBillard6\nQui dit mois de février dit Ligue des Champions. Et la plus prestigieuse des compétitions réserve son lot de chocs entre les écuries du Vieux Continent encore en lices dans cette compétition. Le Borussia Dortmund affrontera par exemple le PSG pour rallier les quarts de finale alors que les champions d’Europe (et du monde) en titre, les Reds, croiseront la route de l’Atlético de Madrid. Mais la plus grosse affiche de ces 8es de finale de la C1 opposera le Manchester City de Pep Guardiola au Real Madrid de Zinedine Zidane.\nAymeric Laporte (25 ans), le défenseur central des Citizens, s’est d’ailleurs exprimé au micro de Téléfoot, ce dimanche, au sujet de cette double confrontation à venir (match aller à Madrid le 28 février, match retour à l’Etihad Stadium le 17 mars) : « c’est une des meilleures équipes du monde actuellement et je pense qu’ils sont favoris par rapport aux années précédentes en Ligue de champions et à leur vécu. Après, on a de très grands joueurs, comme eux, on peut faire mal aussi. Ce peut être une confrontation très disputée ». Les Skyblues, doubles champions d’Angleterre en titre, n’ont jamais dépassé les demi-finales en Ligue des Champions alors que les Merengues ont soulevé la coupe aux grandes oreilles à 13 reprises au cours de leur histoire, un record.\n",
-  audio_url: "http://res.cloudinary.com/quentinlallemand/video/upload/v1581273176/vexytzmn5fkf7xzpygol.mp3",
+  text_to_transcript: "17 septembre 2019. Première journée de la phase de groupes de la Ligue des champions. Alors que le Paris Saint-Germain ne fait qu’une bouchée du Real Madrid (3-0), un exploit a lieu du côté de Salzbourg. Alors que le club autrichien reçoit Genk, un jeune attaquant de 19 ans, Erling Braut Håland inscrit un triplé lors de la victoire des siens (6-1). C’est seulement le troisième joueur de mois de 20 ans de l’Histoire de la compétition à réaliser cet exploit. Seuls Wayne Rooney et Raul l’avaient effectué avant lui. Toute l’Europe est interloquée par la performance et n’a pas fini d’être impressionnée. Dans ce groupe où Salzbourg tient la dragée haute à Liverpool et au Napoli, Håland enfile les buts comme des perles (8 en 5 matches). Il devient ainsi le premier joueur de l’Histoire de la Champions League à marquer lors de ses cinq premiers matches dans la compétition.Ses performances ne passent pas inaperçues bien entendu. D’autant qu’au sein du championnat autrichien, l’attaquant norvégien a les mêmes temps de passage. En 16 matches, il score à 20 reprises dont 4 triplés qui semblent devenir sa marque de fabrique. Le RB Salzbourg devient donc trop petit pour le phénomène. Alors cet hiver, le Borussia Dortmund entame des négociations et fin décembre décroche la signature pour quatre ans d’Håland. Et l’intégration au sein du BVB est express. Pour son premier match, il entre à 30 minutes de la fin sur la pelouse d’Augsbourg. Et alors que son équipe est menée, il inscrit un triplé ! En trois semaines, il écrit l’Histoire en marquant 8 buts en 4 matches et en devenant le premier joueur de Bundesliga à scorer à 7 reprises sur ses 7 premiers tirs cadrés !Le prodige fait honneur à ses deux surnoms : le Monstre et The Manchild (l’homme-enfant) qu’il a obtenu quand il était encore adolescent à Molde. L’un des plus grands clubs de Norvège avait repéré le phénomène très jeune, notamment grâce à son responsable de la cellule de recrutement, John Vik. Débarqué en 2017 en provenance de son club formateur, le Bryne FK, Håland a dû prendre son mal en patience. En pleine croissance, son entraîneur d’alors, un certain Ole Gunnar Solskjaer refuse de le faire jouer afin que son corps se développe dans sa totalité. Bien lui en a pris puisqu’en quelques mois, le fils de l’ancien défenseur de Leeds United, Alf-Inge Håland, prend 12 cm. Du haut de ses désormais 1m94, il peut planer sur le championnat norvégien et empiler les buts (12 en 26 matches en 2018). Des statistiques qui lui ouvrent les portes du Red Bull Salzbourg contre un chèque de 5 M€. La suite on la connait, mais avec le Norvégien il faut se souvenir d’une chose : ce n’est que le début de sa formidable ascension.L’intégralité du focus et du parcours d’Erling Braut Håland est à retrouver dans la vidéo ci-dessus.",
+  audio_url: "http://res.cloudinary.com/quentinlallemand/video/upload/v1581870010/oerijw5gfshlactd73sn.mp3",
   user: users.first,
-  title: "Manchester City : Aymeric Laporte prévient le Real Madrid",
+  title: "Erling Braut Håland, la machine à buts du Borussia Dortmund",
   iso: "fr",
-  text_image: "https://source.unsplash.com/50x50/?abstract",
+  text_image: "http://www.footmercato.net/images/a/focus-sur-erling-braut-haland-l-attaquant-norvegien-de-dortmund_273993.jpg",
   duration: "1:29",
   )
 print ".Audio1. "
@@ -89,7 +89,7 @@ Audio.create!(
   user: users.first,
   title: "Test text no url",
   iso: "fr",
-  text_image: "https://source.unsplash.com/50x50/?abstract",
+  text_image: "https://source.unsplash.com/100x100/?abstract",
   duration: "2:19",
   )
 print ".Audio2."

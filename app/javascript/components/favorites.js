668f452774577afd30a184146bea707442363281
quentinlallemand <quentinlallemand@gmail.com>
2020-02-19 00:33:46 +0100

    add favorites ok

--- app/javascript/components/favorites.js
+++ app/javascript/components/favorites.js
@@ -6,6 +6,8 @@ const favorites = () => {
       const audio = audioFav.dataset.audioId;
       console.log(audio);
       sendData(audio);
+      audioFav.querySelector('i').classList.toggle('far');
+      audioFav.querySelector('i').classList.toggle('fas');
     });
   });
 
@@ -13,16 +15,19 @@ const favorites = () => {
 
   const sendData = (data) => {
     const audio_id = data ;
-    const url = `${ENV['SERVER_ADDRESS']}/api/v1/audios_playlists`;
+    const url = `${ENV['SERVER_ADDRESS']}/api/v1/audios_playlists/`;
     fetch(url, {
       method: 'POST',
       headers: { "Content-Type": "application/json",
       "Accept": "application/json"
     },
     body: JSON.stringify({ "audio_id": audio_id})
-  }).then(response => console.log(response))
+    }).then(response => console.log(response))
+  };
+};
 
-  }
-}
+const redirect = (user) => {
+  window.location= `/users/${user}` ;
+};
 
 export { favorites };
--- app/models/audios_playlist.rb
+++ app/models/audios_playlist.rb
@@ -1,4 +1,5 @@
 class AudiosPlaylist < ApplicationRecord
-  belongs_to :audio, dependent: :destroy
-  belongs_to :playlist, dependent: :destroy, optional: true
+  belongs_to :audio
+  belongs_to :playlist, optional: true
+  validates_uniqueness_of :audio_id, scope: :playlist_id
 end

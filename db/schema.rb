989c5b9d782666a56e28dd9ea6800813483c855e
Pierre Jean Heissat <pjn.heissat@gmail.com>
2020-02-18 20:46:54 +0100

    Travail en cours

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_171952) do
+ActiveRecord::Schema.define(version: 2020_02_18_185537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_02_16_171952) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "playlist_image"
     t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 

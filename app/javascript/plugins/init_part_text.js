04440d747cdd0a196bb800cd5ed14c31ed6f2c6d
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-22 12:17:14 +0100

    play part of text :-)

--- app/javascript/plugins/init_part_text.js
+++ app/javascript/plugins/init_part_text.js
@@ -2,7 +2,11 @@ const part_text = () => {
   const all_text = document.querySelectorAll('.record');
   all_text.forEach((part_text) => {
      part_text.addEventListener("click", (event) => {
-      console.log(part_text);
+      player.currentTime = part_text.dataset.start
+      var playing = document.querySelector('.plyr--playing');
+      if ( !playing ) {
+        document.querySelector('.plyr__control').click();
+      }
      })
   })
 }

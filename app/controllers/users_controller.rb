cee09b527df408afca971fafa3cdc446bef65ec6
quentinlallemand <quentinlallemand@gmail.com>
2020-02-25 03:34:55 +0100

    except not working :(

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,9 +36,13 @@ class UsersController < ApplicationController
     @user = User.find_by(nickname: params[:nickname])
     current_user.follow(@user)
     @activity = @user.create_activity :follow, owner: current_user
-    ActionCable.server.broadcast("activities", {
-      activity_partial: render_to_string(partial: "shared/activity", locals: { activity: @activity })
-    })
+    # binding.pry
+
+    # @current_user.followers.each do |notif|
+      ActionCable.server.broadcast("activities", {
+        activity_partial: render_to_string(partial: "shared/activity", locals: { activity: @activity, user_is_author: false })
+      })
+    # end
     skip_authorization
 
     respond_to :js

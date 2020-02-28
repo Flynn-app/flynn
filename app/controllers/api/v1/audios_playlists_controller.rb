27df0907bbbe292166a2d7cf3d88c12396c3c4d6
quentinlallemand <quentinlallemand@gmail.com>
2020-02-28 23:36:33 +0100

    correction on favorite user logic

--- app/controllers/api/v1/audios_playlists_controller.rb
+++ app/controllers/api/v1/audios_playlists_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::AudiosPlaylistsController < Api::V1::BaseController
   def create
     @audio = Audio.find(params[:audio_id])
-    @playlist = @audio.user.playlists.first
-    @audio_found = AudiosPlaylist.find_by(audio_id: @audio.id, playlist_id: @playlist.id)
     @user = current_user
+    @playlist = @user.playlists.first
+    @audio_found = AudiosPlaylist.find_by(audio_id: @audio.id, playlist_id: @playlist.id)
     skip_authorization
 
     if @audio_found.nil?
